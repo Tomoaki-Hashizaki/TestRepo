5531accfa46d70a80e79c0187592b756d410ebe5
Tomoaki-Hashizaki <tomoakihashi@gmail.com>
2013-09-16 01:34:49 +0900

    changed function's name

--- checkSeq.hs
+++ checkSeq.hs
@@ -4,9 +4,9 @@ digitToBinary   :: Integer -> [Integer]
 digitToBinary 0 =  []
 digitToBinary n =  n `mod` 2 : digitToBinary (n `div` 2)
 
-splitAtOne :: [Integer] -> [Int]
-splitAtOne [] = []
-splitAtOne xs = (length $ takeWhile (< 1) xs) : (splitAtOne $ tail $ dropWhile (< 1) xs)
+countZeros :: [Integer] -> [Int]
+countZeros [] = []
+countZeros xs = (length $ takeWhile (< 1) xs) : (countZeros $ tail $ dropWhile (< 1) xs)
 
 checkSeq :: Integer -> [Int]
-checkSeq = reverse . splitAtOne . digitToBinary
+checkSeq = reverse . countZeros . digitToBinary
